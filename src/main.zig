438a981990daf87ad3f2bbd84cfd615c29ef5570
johan0A <arnaudchesse@outlook.com>
2024-03-18 03:23:37 +0100

    fix set method in GridState

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,12 @@
 const std = @import("std");
 
-const Cell_state = union(enum) {
+const Cell_state_type = enum {
+    snake,
+    fruit,
+    empty,
+};
+
+const Cell_state = union(Cell_state_type) {
     snake: u8,
     fruit: void,
     empty: void,
@@ -16,8 +22,8 @@ fn GridState(comptime size: u16) type {
             // set(this.grid_size / 2, this.grid_size / 2, Cell_state{ .snake = 0 });
         }
 
-        pub fn set(this: @This(), x: u16, y: u16, value: Cell_state) void {
-            this.value_grid[x + y * this.grid_size] = value;
+        pub fn set(this: *@This(), x: u16, y: u16, value: Cell_state) void {
+            this.*.value_grid[x + y * this.grid_size] = value;
         }
 
         pub fn get(this: @This(), x: u16, y: u16) Cell_state {
@@ -56,16 +62,38 @@ fn Range(comptime size: u16) type {
     };
 }
 
-test "range-contains" {
-    std.debug.print("A", .{});
+const ResponseType = enum {
+    okay,
+    not_okay,
+};
+
+const Response = union(ResponseType) {
+    okay: void,
+    not_okay: usize,
+};
+
+test "AAA" {
+    // This will fail to compile - error: no member named 'not_okay' in enum 'ResponseType'
+    const resp = Response.okay;
+    // resp = Response{ .not_okay = 1 };
+
+    switch (resp) {
+        .okay => std.debug.print("okay", .{}),
+        .not_okay => std.debug.print("no okay", .{}),
+    }
+
+    // // This variant will succeed
+    // var resp = Response{ .not_okay = 1 };
+    // resp = Response.okay;
 }
 
 pub fn main() !void {
     var grid = GridState(16){};
     grid.init();
-    const unionex = Cell_state{ .snake = 10 };
-    std.debug.print("{}", .{unionex});
-    // grid.set(5, 5, Cell_state{ .snake = 0 });
-    // grid.value_grid[1] = Cell_state{.empty};
+    std.debug.print("\n", .{});
+    // const unionex = Cell_state{ .snake = 10 };
+    // std.debug.print("{}", .{unionex});
+    grid.set(5, 5, Cell_state.empty);
+    // grid.value_grid[1] = Cell_state.empty;
     grid.show_grid();
 }
